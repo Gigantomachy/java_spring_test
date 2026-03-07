69f2c4a40bf31ab2d95ce32ad7162f84f0907fac
Steven Chu <steven.chu.1997@gmail.com>
2026-03-06 20:16:43 -0500

    4.1: Setting Up JPA with H2 In-Memory Database

---
+++ 4_1/hellospring/src/main/java/com/exercises/hellospring/controller/BookController.java
@@ -0,0 +1,72 @@
+package com.exercises.hellospring.controller;
+
+import com.exercises.hellospring.service.BookService;
+
+import jakarta.validation.Valid;
+
+import com.exercises.hellospring.dto.BookRequestDTO;
+import com.exercises.hellospring.dto.BookResponseDTO;
+import com.exercises.hellospring.dto.PagedResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+// import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+    
+    BookService bookService;
+
+    public BookController(BookService service) {
+        this.bookService = service;
+    }
+
+    @GetMapping
+    public List<BookResponseDTO> getBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookResponseDTO> getBook(@PathVariable Long id) {
+        return ResponseEntity.status(200).body(bookService.getBookById(id));
+    }
+
+    @GetMapping("/search")
+    public PagedResponse<BookResponseDTO> getBookSearch(
+                                                        @RequestParam(required = false)     String author, 
+                                                        @RequestParam(required = false)     String title, 
+                                                        @RequestParam(defaultValue = "0")   int page, 
+                                                        @RequestParam(defaultValue = "10")  int size) {
+        return bookService.getBookSearch(author, title, page, size);
+    }
+    
+    // without @Valid, the validator won't run - even if fields are annotated in Book
+    @PostMapping
+    public ResponseEntity<BookResponseDTO> createBook(@Valid @RequestBody BookRequestDTO book) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(bookService.createBook(book));
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<BookResponseDTO> replaceBook(@PathVariable Long id, @Valid @RequestBody BookRequestDTO book) {
+        return ResponseEntity.ok(bookService.updateBook(id, book));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
+        bookService.deleteBook(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ 4_1/hellospring/src/main/java/com/exercises/hellospring/service/BookService.java
@@ -0,0 +1,17 @@
+package com.exercises.hellospring.service;
+
+import java.util.List;
+
+import com.exercises.hellospring.dto.BookRequestDTO;
+import com.exercises.hellospring.dto.BookResponseDTO;
+import com.exercises.hellospring.dto.PagedResponse;
+
+public interface BookService {
+    
+    List<BookResponseDTO> getAllBooks();
+    BookResponseDTO getBookById(Long id);
+    BookResponseDTO createBook(BookRequestDTO book);
+    BookResponseDTO updateBook(Long id, BookRequestDTO book);
+    void deleteBook(Long id);
+    PagedResponse<BookResponseDTO> getBookSearch(String author, String title, int page, int size);
+}
